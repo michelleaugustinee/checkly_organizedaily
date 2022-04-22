5d05a76071faf5e0fb9a96c7476ff598cbf6000b
hongi <hongi.535200082@stu.untar.ac.id>
2022-04-22 22:05:03 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(
-    initialRoute: '/list',
+    initialRoute: '/login',
     routes: {
       '/login': (context) => Login(),
       '/home': (context) => Home(),
